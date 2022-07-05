1fdac62acfe61c800594d36d67b0c7c163f1661f
Oscar McCullough <odm3productions@gmail.com>
2022-07-04 22:33:12 -0400

    Removed getKey FN

--- getEvents/index.ts
+++ getEvents/index.ts
@@ -1,13 +1,17 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import { _CONSTANTS_ } from "../Constants";
 import fetch from "node-fetch";
+import { DefaultAzureCredential } from "@azure/identity";
+import { SecretClient } from "@azure/keyvault-secrets";
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const { authorization } = req.headers;
+    const credential = new DefaultAzureCredential();
+    const url = _CONSTANTS_.KEYVAULT_URL || "";
+    const client = new SecretClient(url, credential);
+    const secret = await client.getSecret(_CONSTANTS_.SECRET_NAME);
     const REURL = _CONSTANTS_.ROBOTEVENTSURL || "";
     const headers = {
-        'Authorization': `${authorization}`
+        'Authorization': `Bearer ${secret.value}`
      };
      const fetchArrays = _CONSTANTS_.SEASON_IDS.map((id) => {
         return fetch(`${REURL}/${id}/events?per_page=${_CONSTANTS_.PAGE_LIMIT}`, { headers });
